46040f40930415d9bbfcdfe8f28cccce849b50d2
Chloé Briquet <ch.briquet@outlook.com>
2019-12-12 21:23:52 +0100

    create Code Entity & Fixtures

---
+++ src/DataFixtures/CodeFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Code;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CodeFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $code = new Code();
+        $code->setName('code');
+
+        $manager->persist($code);
+        $manager->flush();
+    }
+}
