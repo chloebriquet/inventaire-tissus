3edab08d3b6787ce96f45480f4a5fcdfbe87fe4f
Chloé Briquet <ch.briquet@outlook.com>
2019-12-12 21:21:06 +0100

    add webcolors to config file and create & Color Entity & Fixtures

---
+++ src/DataFixtures/ColorFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Color;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ColorFixtures extends Fixture
+{
+    private $webcolors;
+
+    public function __construct(array $webcolors)
+    {
+        $this->webcolors = $webcolors;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $color = new Color();
+            $color->setCode($this->webcolors[$i]);
+            $color->setName($this->webcolors[$i]);
+
+            $manager->persist($color);
+        }
+
+        $manager->flush();
+    }
+}
