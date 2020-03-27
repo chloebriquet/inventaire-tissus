58470c3b05eac01c9855867184b824f20c902cb6
Chloé Briquet <ch.briquet@outlook.com>
2020-03-27 19:35:22 +0100

    add softdelete

---
+++ src/Listener/DeletedListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listener;
+
+use Doctrine\ORM\Event\OnFlushEventArgs;
+
+class DeletedListener
+{
+    public function onFlush(OnFlushEventArgs $eventArgs)
+    {
+        $em = $eventArgs->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        foreach ($uow->getScheduledEntityDeletions() as $entity) {
+            if (
+                method_exists($entity, 'getDeletedAt')
+                && !$entity->getDeletedAt() instanceof \Datetime
+            ) {
+                $entity->setDeletedAt(new \DateTime());
+
+                try {
+                    $em->persist($entity);
+                    $uow->recomputeSingleEntityChangeSet($em->getClassMetadata(get_class($entity)), $entity);
+                } catch (\Exception $exception) {
+                    continue;
+                }
+            }
+        }
+    }
+}
